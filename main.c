dae354c653a8d1271371fc13ae678959809cf3fa
caspeer <mr.ash_x7@protonmail.ch>
2025-04-22 02:01:02 +0100

    clipping and trimming unnecessary code

--- main.c
+++ main.c
@@ -9,35 +9,29 @@ enum Symbol{
 };
 
 int check_winner(int positions[],int sym){
-	int winner = 0;
 	int i;
-	int counter;
 	for (i = 1;i < 10;i= i+3){
 		if (positions[i-1] == sym && positions[i] == sym && positions[i+1] == sym){
-			winner = 1;
-			return winner;
+			return 1;
 		}
 	}
 	for (i = 0;i<=4;i++){
 		if (positions[i] == sym && positions[i+3] == sym && positions[i+6] == sym){
-			winner = 1;
-			return winner;
+			return 1;
 		}
 	}
 	if (positions[0] == sym && positions[4] == sym && positions[8] == sym){
-		winner = 1;
-		return winner;
+		return 1;
 	}
 	else if(positions[2] == sym && positions[4] == sym && positions[6] == sym){
-		winner = 1;
-		return winner;
+		return 1;
 	}
-	return winner;
+	return 0;
 }
 
 
-int redraw(int positions[],char sym){
-
+void redraw(int positions[])
+{
 	printf("|---|---|---|\n");
 	int status = 1;
 	int i;
@@ -69,7 +63,6 @@ int redraw(int positions[],char sym){
 		}
 
 	}
-
 }
 
 
@@ -77,25 +70,18 @@ int redraw(int positions[],char sym){
 int main(void)
 {
 	int positions[9];
-	int i2;
-	for (i2 = 0; i2 < 10; i2++){
-		positions[i2] = 0;
-	}
 	int i;
-	int c = 1;
+	for (i = 0; i < 10; i++){
+		positions[i] = 0;
+	}
 	int turn;
 	printf("TIC TAC TOE\n");
-	printf("|---|---|---|\n");
-	for (i = 0; i < 3;i++){
-		printf("|%3d|%3d|%3d|\n",c,c+1,c+2);
-		printf("|---|---|---|\n") ;
-		c = c + 3;
-	}
+	redraw(positions);
 	//choosing X or O?
 	char user_symbol, my_symbol;
 	printf(" X or O ?\n");
 	scanf("%c", &user_symbol);
-	
+
 	switch (user_symbol) {
 	case X: {
 		my_symbol = O;
@@ -119,24 +105,22 @@ int main(void)
 		return 0;
 	}
 
-
 	// who is gonna start first?
 	srand(time(NULL));
 	turn = rand() % 2;
 
 	int counter = 0;
-	short int pos;
+	unsigned char pos;
 	while (counter <= 9) {
 		if (turn){
-			
 			printf("Your Turn\n");
 			printf("pos:");
-			scanf("%d", &pos);
-			if ((0 < pos < 10) && (positions[pos-1] != user_symbol) && (positions[pos-1] != my_symbol)){
+			scanf("%hhu", &pos);
+			if ((0 < pos && pos < 10) && (positions[pos-1] != user_symbol) && (positions[pos-1] != my_symbol)){
 				turn = 0;
 				positions[pos-1] = user_symbol;
 				counter++;
-				redraw(positions,user_symbol);
+				redraw(positions);
 		 		if (counter >= 5 && check_winner(positions,user_symbol) ){
 					printf("YOU WON\n");
 					break;
@@ -156,19 +140,15 @@ int main(void)
 					positions[k] = my_symbol;
 					break;
 				}
-
 			}
 			counter++;
 			sleep(1);
-			redraw(positions,my_symbol);
+			redraw(positions);
 			if (counter >= 5 && check_winner(positions,my_symbol) ){
 				printf("I WON\n");
 				break;
 			}
-
-
 		}
 	}
-	return 1;
+	return 0;
 }
-
